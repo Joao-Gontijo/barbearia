7e85ad85c19539b1432efd67ca6a280430cbe543
Joao-Gontijo <jvgontijo166@gmail.com>
2021-08-20 12:56:57 -0300

    update-agenda
    Começo da página de agenda.
    Fix dos icons com link na app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <Cabecalho/>
   </div>
 </template>
--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -1,13 +1,65 @@
 <template>
     <div class="container">
-        <h1>Aqui vai estar a agenda</h1>
-        <input type="datetime-local">
+        <div class="column">
+            <label>Campo 1</label>
+            <input class="column" type="datetime-local">
+            <label>Campo 2</label>
+            <input class="input" type="text">
+            <label>Campo 3</label>
+            <input class="input" type="text">
+            <label>Barbeiro</label>
+            
+            <div class="dropdown">
+                <div class="dropdown-trigger">
+                    <button class="button" @click="ligarDropdown(), listar()" aria-haspopup="true" aria-controls="dropdown-menu">
+                    <span>Dropdown button</span>
+                    <span class="icon is-small">
+                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                    </button>
+                </div>
+                <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                    <div class="dropdown-content">
+                    <div v-for="barbeiro in barbeiros" :key="barbeiro.id">
+                        <a href="#" class="dropdown-item">
+                            {{barbeiro.nome}}
+                        </a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
 
+import Barbeiro from '@/services/barbeiros'
+
+export default {
+    data(){
+        return{
+            barbeiros:[]
+        }
+    },
+    methods:{
+        ligarDropdown() {
+            var menu = document.querySelector('.dropdown');
+            // var burger = document.querySelector('.navbar-burger');
+            // burger.addEventListener('click', function() {
+            
+            menu.classList.toggle('is-active');
+            // burger.classList.toggle('is-active');
+            // });
+
+        },
+        listar(){
+            Barbeiro.listar().then(resposta => {    
+            this.barbeiros = resposta.data;
+            console.log(this.barbeiros);
+            })
+        }
+    }
 }
 </script>
 
